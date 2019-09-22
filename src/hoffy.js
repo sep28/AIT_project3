4d19c0501d8be1c7ffda49c6465a4af2dce003e6
Sep <sepmacbook@10-16-139-197.dynapool.wireless.nyu.edu>
2019-09-22 19:04:30 -0400

    saving changes to hoffy function

--- src/hoffy.js
+++ src/hoffy.js
@@ -1,1 +1,55 @@
 // hoffy.js
+
+function sum(...nums) {
+	let sum = 0;
+	if (nums.length === 0) {
+		return sum;
+	}
+	else {
+		const reduceSum = (accumulator, currentValue) => {accumulator + currentValue};
+		sum = nums.reduce(reduceSum, 0);
+		return sum;
+	}
+}
+
+
+function callFn(fn, n, arg) {
+	if (n === 0) {
+		return;
+	}
+
+	else {
+		fn(arg);
+		callFn(fn, n-1, arg);
+	}
+}
+
+
+function betterCallFn(fn, n, ...argsn) {
+	if (n === 0) {
+		return;
+	}
+	else {
+		fn(...argsn);
+		betterCallFn(fn, n-1, ...argsn);
+	}
+}
+
+
+function opposite(oldFn) {
+	return function(arg1) {
+		const current = oldFn(arg1);
+		return (!current);
+	}
+}
+
+
+
+
+
+module.exports = {
+	sum: sum,
+	callFn: callFn,
+	betterCallFn: betterCallFn,
+	opposite: opposite
+}
